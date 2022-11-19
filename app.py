3e75550a1700eb7a53af69d9ddd3ffbd2b45b739
Tara Denniston <taradenniston@gmail.com>
2022-11-19 16:37:14 -0500

    added route and logic to say thanks once survey is completed

--- app.py
+++ app.py
@@ -9,30 +9,50 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 responses = []
-q_num = [0, len(s.satisfaction_survey.questions)]
+q_num = [1, len(s.satisfaction_survey.questions)]
 
 @app.route('/')
 def home_page():
+    """Provide survey title and instructions to home page template"""
+    responses.clear()
+    q_num[0] = 1
     title = s.satisfaction_survey.title
     instructions = s.satisfaction_survey.instructions
+    print(f'new session: responses {responses}, q_num {q_num}')
     return render_template('home.html', title=title, instructions=instructions)
 
 @app.route('/questions/<int:num>')
 def display_question(num):
+    """Provide question/choices based on number"""
+    idx = num - 1
     title = s.satisfaction_survey.title
-    question = s.satisfaction_survey.questions[num].question
-    choices = s.satisfaction_survey.questions[num].choices
+    question = s.satisfaction_survey.questions[idx].question
+    choices = s.satisfaction_survey.questions[idx].choices
+    print(f'displaying question {num}')
+    print(f'{responses} {q_num}')
     return render_template('questions.html', title=title, question=question, \
         choices=choices, num=num)
 
 @app.route('/answer', methods=["POST"])
 def record_answer():
+    """Append user's posted answer to list of responses and figure out next page"""
     answer = request.form['question']
-    print(answer)
+    print(f'question {q_num[0]} of {q_num[1]} response: {answer}')
     responses.append(answer)
     q_num[0] += 1
+    print(f'{responses} {q_num}')
+    
     url = f'/questions/{q_num[0]}'
-    return redirect(url)
+
+    if q_num[0] <= q_num[1]:
+        return redirect(url)
+    else:
+        return redirect('/thanks')
+
+@app.route('/thanks')
+def say_thanks():
+    return render_template('thanks.html')
+    
 
 
 
