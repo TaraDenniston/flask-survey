67205930b0860a3480fda7c91264e2a4e92b2d5c
Tara Denniston <taradenniston@gmail.com>
2022-11-19 13:11:20 -0500

    added route for questions

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import surveys as s
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -15,3 +15,13 @@ def home_page():
     title = s.satisfaction_survey.title
     instructions = s.satisfaction_survey.instructions
     return render_template('home.html', title=title, instructions=instructions)
+
+@app.route('/questions/<int:num>')
+def display_question(num):
+    title = s.satisfaction_survey.title
+    question = s.satisfaction_survey.questions[num].question
+    choices = s.satisfaction_survey.questions[num].choices
+    return render_template('questions.html', title=title, question=question, \
+        choices=choices, num=num)
+
+
